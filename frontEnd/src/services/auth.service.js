a5d7b5207676406fe83e71185aa23a3ac687de96
AfrodytaP <59373204+AfrodytaP@users.noreply.github.com>
2024-07-03 18:56:36 +0100

    Update auth.service.js

--- frontEnd/src/services/auth.service.js
+++ frontEnd/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `http://0.0.0.0:8000/auth`;
+const API_URL = `https://api.render.com/deploy/srv-cq2lf2t6l47c73b8s9ng?key=LGGTkxe1VBA/auth`;
 // const API_URL = process.env.REACT_APP_AUTH_API_URL;
 
 const register = async (username, email, password) => {
